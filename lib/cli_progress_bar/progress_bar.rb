d7d500fe8e10769e5b6a5d811f1c6099a54368f2
michaeltelford <michael.telford@live.com>
2025-08-19 15:37:32 +0100

    Fixed issue with ItemsProgressBar.increment

--- lib/cli_progress_bar/progress_bar.rb
+++ lib/cli_progress_bar/progress_bar.rb
@@ -1,5 +1,5 @@
 
-# Parent progress bar class to be inherited from only
+# Parent progress bar class to be inherited from only.
 class CLIProgressBar::ProgressBar
   LOG_AT_ALL_PERCENTAGES = (1..100).to_a.freeze
   LOG_AT_TEN_PERCENTAGES = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100].freeze
@@ -18,13 +18,23 @@ class CLIProgressBar::ProgressBar
 
   private
 
-  # |------------------->       |  70% of 2.3 MiB
-  def write_to_stream
+  # Percent: [------------------->       ]  70% of 2.3 MiB
+  # Items:   [------------------>        ]  8 of 12 repos (66%)
+  def write_to_stream(items: nil)
     return unless @log_at.include?(@progress)
 
     completed_length = (@bar_length * @progress / 100).to_i
     remaining_length = @bar_length - completed_length
-    line = "[#{'-' * (completed_length-1)}>#{' ' * remaining_length}]  #{@progress}% of #{@of}"
+
+    if items
+      totals = "#{items} of #{@of} (#{@progress}%)"
+    else
+      totals = "#{@progress}% of #{@of}"
+    end
+
+    line = '[%s>%s]  %s' % [
+      '-' * (completed_length-1), ' ' * remaining_length, totals
+    ]
 
     @stream.puts(line)
 
